c7c1ebab27e859fb92b1558d2eac54d46eeafeae
Robert burner Schadek <robert.schadek@informatik.uni-oldenburg.de>
2024-09-15 13:51:55 +0200

    save

--- presentation/example.d
+++ presentation/example.d
@@ -47,21 +47,15 @@ Definition parseDefinitionImpl() {
 	if(this.firstOperationDefinition()) {
 		OperationDefinition op = this.parseOperationDefinition();
 
-		return new Definition(DefinitionEnum.O
-			, op
-		);
+		return new Definition(DefinitionEnum.O, op);
 	} else if(this.firstFragmentDefinition()) {
 		FragmentDefinition frag = this.parseFragmentDefinition();
 
-		return new Definition(DefinitionEnum.F
-			, frag
-		);
+		return new Definition(DefinitionEnum.F, frag);
 	} else if(this.firstTypeSystemDefinition()) {
 		TypeSystemDefinition type = this.parseTypeSystemDefinition();
 
-		return new Definition(DefinitionEnum.T
-			, type
-		);
+		return new Definition(DefinitionEnum.T, type);
 	}
 	auto app = appender!string();
 	formattedWrite(app,
@@ -72,11 +66,15 @@ Definition parseDefinitionImpl() {
 		__FILE__, __LINE__
 		, ["lcurly -> SelectionSet","mutation -> OperationType","query -> OperationType","subscription -> OperationType","fragment","directive -> DirectiveDefinition","enum_ -> TypeDefinition","extend -> TypeExtensionDefinition","input -> TypeDefinition","interface_ -> TypeDefinition","scalar -> TypeDefinition","schema -> SchemaDefinition","stringValue -> Description","type -> TypeDefinition","union_ -> TypeDefinition"]
 	);
+}
 
+bool firstOperationType() const pure @nogc @safe {
+	return this.lex.front.type == TokenType.query
+		 || this.lex.front.type == TokenType.mutation
+		 || this.lex.front.type == TokenType.subscription;
 }
 
 class Visitor : ConstVisitor {
-@safe :
 	void enter(Definition obj) {}
 	void exit(Definition obj) {}
 
@@ -95,4 +93,47 @@ class Visitor : ConstVisitor {
 		}
 		exit(obj);
 	}
+}
+
+InlineFragment:
+    TDS: [on_, name#tc, Directives#dirs, SelectionSet#ss]
+    TS: [on_, name#tc, SelectionSet#ss]
+    DS: [Directives#dirs, SelectionSet#ss]
+    S: [SelectionSet#ss]
+
+InlineFragment parseInlineFragmentImpl() {
+	if(this.lex.front.type == TokenType.on_) {
+		this.lex.popFront();
+		if(this.lex.front.type == TokenType.name) {
+			Token tc = this.lex.front;
+			this.lex.popFront();
+			if(this.firstDirectives()) {
+				Directives dirs = this.parseDirectives();
+				if(this.firstSelectionSet()) {
+					SelectionSet ss = this.parseSelectionSet();
+
+					return new InlineFragment(InlineFragmentEnum.TDS, tc, dirs, ss);
+				}
+				throw new ParseException(["lcurly"]);
+			} else if(this.firstSelectionSet()) {
+				SelectionSet ss = this.parseSelectionSet();
+
+				return new InlineFragment(InlineFragmentEnum.TS, tc, ss);
+			}
+			throw new ParseException(["at -> Directive","lcurly"]);
+		}
+		throw new ParseException(["name"]);
+	} else if(this.firstDirectives()) {
+		Directives dirs = this.parseDirectives();
+		if(this.firstSelectionSet()) {
+			SelectionSet ss = this.parseSelectionSet();
+
+			return new InlineFragment(InlineFragmentEnum.DS, dirs, ss);
+		}
+		throw new ParseException(["lcurly"]);
+	} else if(this.firstSelectionSet()) {
+		SelectionSet ss = this.parseSelectionSet();
+		return new InlineFragment(InlineFragmentEnum.S, ss);
+	}
+	throw new ParseException(["on_","at -> Directive","lcurly"]);
 }
